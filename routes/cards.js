2203d7cfef9527f9f755b4222c7eea362a13855a
Sergey Kazarinov <kazarinov092@gmail.com>
2022-09-18 15:17:09 +0500

    feat(cards and user): add methods update user and set likes

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,10 @@
 const cards = require('express').Router();
-const {getCards, createCard, deleteCard} = require('../controllers/cards');
+const {getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 cards.get('/cards', getCards);
 cards.post('/cards', createCard);
 cards.delete('/cards/:cardId', deleteCard);
-
+cards.put('/cards/:cardId/likes', likeCard);
+cards.delete('/cards/:cardId/likes', dislikeCard );
 
 module.exports = cards;
