ccf768165bbf5a6a4370fb6985616dfd711ee606
Sergey Kazarinov <kazarinov092@gmail.com>
2022-09-29 13:37:20 +0500

    feat(validation): add celebrate validation

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { notFoundController } = require('./errors/notFoundController');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const err = require('./middlewares/error');
+const { loginUserValidation, createUserValidation } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,8 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', loginUserValidation, login);
+app.post('/signup', createUserValidation, createUser);
 
 app.use(auth);
 
@@ -30,6 +32,7 @@ app.use('/', userRouter);
 app.use('/', cardRouter);
 
 app.use('*', notFoundController);
+app.use(errors());
 app.use(err);
 
 app.listen(PORT, () => {
---
+++ middlewares/validation.js
@@ -0,0 +1,63 @@
+const { celebrate, Joi } = require('celebrate');
+
+const loginUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+});
+
+const createUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
+    about: Joi.string().min(2).max(30).default('Исследователь'),
+    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+});
+
+const getUserByIdValidation = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24),
+  }),
+});
+
+const updateUserValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+});
+
+const updateAvatarUserValidation = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string(),
+  }),
+});
+
+const createCardValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required(),
+  }),
+});
+
+const searchCardIdValidation = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24),
+  }),
+});
+
+module.exports = {
+  getUserByIdValidation,
+  updateUserValidation,
+  updateAvatarUserValidation,
+  loginUserValidation,
+  createUserValidation,
+  createCardValidation,
+  searchCardIdValidation,
+};
